16ef7072037f7c292c852b2916a4c468ac5d7370
Patrick Garrett Pavesi <patrickgpavesi@gmail.com>
2015-05-27 17:20:50 -0400

    Got some feedback from Tomas. Cleaned up several things in the views. Made a new username and password for the database and inserted them with enviorment variables.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,23 @@
   Category.find_or_create_by(name: category)
 end
 
-Video.create(title: "Futurama", description: "A pizza dilvery boy is flung into the year 3000! One of the best animated comedies ever made.", small_cover_url: "futurama.jpg", category_id: 2)
+Video.create!(title: "Futurama",
+             description: "A pizza dilvery boy is flung into the year 3000! One of the best animated comedies ever made.",
+             small_cover_url: "futurama.jpg",
+             category_id: 2)
 
-Video.create(title: "South Park", description: "The legendary animated series by Matt Stone and Trey Parker.", small_cover_url: "south_park.jpg", category_id: 2)
+Video.create!(title: "South Park",
+             description: "The legendary animated series by Matt Stone and Trey Parker.",
+             small_cover_url: "south_park.jpg",
+             category_id: 2)
 
-Video.create(title: "Family Guy", description: "An average comedy the you'll watch at 3am on Adult Swim when you can't sleep sometimes.", small_cover_url: "family_guy.jpg", category_id: 2)
+Video.create!(title: "Family Guy",
+             description: "An average comedy that you'll watch at 3am on Adult Swim when you can't sleep.",
+             small_cover_url: "family_guy.jpg",
+             category_id: 2)
 
-Video.create(title: "Monk", description: "That guy from Glaxy Quest is akward and solves mysteries.", small_cover_url: "monk.jpg", large_cover_url: "monk_large.jpg", category_id: 3)
+Video.create!(title: "Monk",
+             description: "That guy from Glaxy Quest is akward and solves mysteries.",
+             small_cover_url: "monk.jpg",
+             large_cover_url: "monk_large.jpg",
+             category_id: 3)
