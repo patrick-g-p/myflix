af1d465e5e821ffb64295389cbb346393eaa6377
Patrick Garrett Pavesi <patrickgpavesi@gmail.com>
2015-05-29 22:48:59 -0400

    added search by title method using tdd

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,8 @@ class Video < ActiveRecord::Base
   belongs_to :category
 
   validates_presence_of :title, :description
+
+  def self.search_by_title(search)
+    self.where("title LIKE ?", "%#{search.capitalize}%" )
+  end
 end
--- spec/models/videos_spec.rb
+++ spec/models/videos_spec.rb
@@ -5,4 +5,22 @@ describe Video do
 
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
+
+  it 'returns no videos from the database' do
+    videos = Video.search_by_title('you get nothing!')
+    expect(videos.size).to eq(0)
+  end
+  it 'returns one video from the database' do
+    Video.create(title: 'Samurai Jack', description: 'A samurai is flung into the future.')
+
+    videos = Video.search_by_title('Samurai')
+    expect(videos.size).to eq(1)
+  end
+  it 'returns multiple videos from the databse' do
+    Video.create(title: 'Samurai Jack', description: 'A samurai is flung into the future.')
+    Video.create(title: 'Samurai Champloo', description: 'Hip-hop soundtrack and the search for the samurai who smells of sunflowers.')
+
+    videos = Video.search_by_title('Samurai')
+    expect(videos.size).to be >= 2
+  end
 end
