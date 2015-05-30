c414780de15bbd7205a80c997fdf3a64db29eff7
Patrick Garrett Pavesi <patrickgpavesi@gmail.com>
2015-05-30 12:30:04 -0400

    Updated the view to make use of the recent_videos method. Also updated the seed with more videos to do a proper sanity test.

--- spec/models/categories_spec.rb
+++ spec/models/categories_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Category do
   it { should have_many(:videos) }
 
-  describe 'recent_videos' do
+  describe '#recent_videos' do
     before :each do
       @category = Category.create(name: 'Science Fiction')
     end
@@ -11,13 +11,15 @@ describe Category do
     it 'returns an empty array if there are no videos in the category' do
       expect(@category.recent_videos).to eq([])
     end
-    it 'returns an array of less than 6 videos with the most recently created first' do
+
+    it 'returns an array of all videos in the category if there are less than 6 with the most recently created first' do
       the_matrix = Video.create(title: 'The Matrix', description: 'Woah...', category: @category)
       star_wars = Video.create(title: 'Star Wars', description: 'A long time ago in a galaxy far, far away...', category: @category)
 
       expect(@category.recent_videos).to eq([star_wars, the_matrix])
     end
-    it 'returns an array of only 6 videos if there are more than 6 in the category with the most recently created first' do
+
+    it 'returns an array of only the 6 most recent videos if there are more than 6 videos in the category' do
       the_matrix = Video.create(title: 'The Matrix', description: 'Woah...', category: @category)
       star_wars = Video.create(title: 'Star Wars', description: 'A long time ago in a galaxy far, far away...', category: @category)
       star_trek = Video.create(title: 'Star Trek', description: 'Not as good as Star Wars ;-)', category: @category)
@@ -26,7 +28,7 @@ describe Category do
       stargate = Video.create(title: 'Stargate', description: 'The film that laid the foundation for the long running tv series.', category: @category)
       predator = Video.create(title: 'Predator', description: 'Arnold Schwarzenegger takes on a deadly alien hunter in the middle of the jungle. You\'ll never get bored of this movie.', category: @category)
 
-      expect(@category.recent_videos).to eq([predator, stargate, battlestar, farscape, star_trek, star_wars])
+      expect(@category.recent_videos).not_to include([the_matrix])
     end
   end
 end
