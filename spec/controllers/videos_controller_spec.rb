59951e89130814a45b991912406d4d1dede9c9db
Patrick Garrett Pavesi <patrickgpavesi@gmail.com>
2015-06-01 16:33:03 -0400

    refactored the videos controller spec

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -2,55 +2,31 @@ require 'spec_helper'
 
 describe VideosController do
 
-  context 'user is logged in' do
-    before(:each) do
+  let!(:robocop) {Fabricate(:video, title: 'RoboCop')}
+
+  describe 'GET show' do
+    it 'sets the @video instance variable if logged in' do
       session[:user_id] = Fabricate(:user).id
+      get :show, id: robocop.id
+      expect(assigns(:video)).to eq(robocop)
     end
 
-    let!(:robocop) {Fabricate(:video, title: 'RoboCop')}
-
-    describe 'GET show' do
-      it 'sets the @video instance variable' do
-        get :show, id: robocop.id
-        expect(assigns(:video)).to eq(robocop)
-      end
-      it 'renders the :show template' do
-        get :show, id: robocop.id
-        expect(response).to render_template :show
-      end
-    end
-
-    describe 'GET search' do
-      it 'sets the @search_results instance variable with any videos it finds' do
-        get :search, search: 'ro'
-        expect(assigns(:search_results)).to eq([robocop])
-      end
-      it 'returns an empty array if there are no matching videos' do
-        get :search, search: 'the empire strikes back'
-        expect(assigns(:search_results)).to eq([])
-      end
-      it 'renders the search template' do
-        get :search, search: "ro"
-        expect(response).to render_template :search
-      end
+    it 'redirects to login page when not authenticated' do
+      get :show, id: robocop.id
+      expect(response).to redirect_to(login_path)
     end
   end
 
-  context "user isn't logged in" do
-    let!(:star_wars) {Fabricate(:video, title: 'Star Wars: A New Hope')}
-
-    describe 'GET show' do
-      it 'redirects to login page when not authenticated' do
-        get :show, id: star_wars.id
-        expect(response).to redirect_to(login_path)
-      end
+  describe 'GET search' do
+    it 'sets the @search_results instance variable with any videos it finds' do
+      session[:user_id] = Fabricate(:user).id
+      get :search, search: 'ro'
+      expect(assigns(:search_results)).to eq([robocop])
     end
 
-    describe 'GET search' do
-      it 'redirects to login page when not authenticated' do
-        get :search, search: 'st'
-        expect(response).to redirect_to(login_path)
-      end
+    it 'redirects to login page when not authenticated' do
+      get :search, search: 'ro'
+      expect(response).to redirect_to(login_path)
     end
   end
 
