931c66c3d2ecd285c1057bd7e2d303ec8e6b676f
Patrick Garrett Pavesi <patrickgpavesi@gmail.com>
2015-06-17 19:04:07 -0400

    view and new route for updating the queue

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Myflix::Application.routes.draw do
   get 'my_queue', to: 'queue_items#index'
 
   resources :queue_items, only: [:create, :destroy]
+  post 'update_queue', to: 'queue_items#update_queue'
 
   resources :videos, only: [:index, :show] do
     collection do
