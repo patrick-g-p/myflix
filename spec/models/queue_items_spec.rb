ddfa076ee5f4904fac7e804343c6c41c869a7def
Patrick Garrett Pavesi <patrickgpavesi@gmail.com>
2015-06-19 18:51:56 -0400

    updated shoulda matcher to include only_integer

--- spec/models/queue_items_spec.rb
+++ spec/models/queue_items_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe QueueItem do
   it { should belong_to(:user) }
   it { should belong_to(:video) }
-  it { should validate_numericality_of(:list_position) }
+  it { should validate_numericality_of(:list_position).only_integer }
 
   let(:user) {Fabricate(:user)}
   let(:category) {Fabricate(:category)}
