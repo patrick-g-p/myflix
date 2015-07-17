ccd3b796006763fa9688cbd523c03fa03f6bcd66
Patrick Garrett Pavesi <patrickgpavesi@gmail.com>
2015-07-17 10:56:03 -0400

    Feedback from Brandon: fixed up some style issues in the specs. Also cleaned up the searchbar in the header

--- spec/controllers/forgotten_passwords_controller_spec.rb
+++ spec/controllers/forgotten_passwords_controller_spec.rb
@@ -2,21 +2,19 @@ require 'spec_helper'
 
 describe ForgottenPasswordsController do
   describe 'POST create' do
-    after {ActionMailer::Base.deliveries.clear}
+    after { ActionMailer::Base.deliveries.clear }
 
     context 'user in database' do
-      let(:a_user) {Fabricate(:user)}
+      let(:a_user) { Fabricate(:user) }
 
-      before(:each) do
-        post :create, email: a_user.email
-      end
+      before(:each) { post :create, email: a_user.email }
 
       it 'redirects to the confirmation page' do
         expect(response).to redirect_to forgot_password_confirm_path
       end
 
       it 'sends the password reset email' do
-        expect(ActionMailer::Base.deliveries).to_not be_empty
+        expect(ActionMailer::Base.deliveries).to be_present
       end
 
       it 'sends the email to the correct user' do
@@ -25,14 +23,12 @@ describe ForgottenPasswordsController do
       end
 
       it 'sets the users password reset token' do
-        expect(a_user.reload.password_reset_token).to_not be_nil
+        expect(a_user.reload.password_reset_token).to be_present
       end
     end
 
     context 'user not in database' do
-      before(:each) do
-        post :create, email: 'ilikemovies@gmail.com'
-      end
+      before(:each) { post :create, email: 'ilikemovies@gmail.com' }
 
       it 'still redirects to the confirmation page' do
         expect(response).to redirect_to forgot_password_confirm_path
