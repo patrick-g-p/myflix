ed7ecc43d072c1a9cd48604b69bbf8c0be9c5548
Patrick Garrett Pavesi <patrickgpavesi@gmail.com>
2015-08-13 18:03:48 -0400

    minor refactor of the wrapper spec

--- spec/models/stripe_wrapper_spec.rb
+++ spec/models/stripe_wrapper_spec.rb
@@ -9,41 +9,43 @@ describe StripeWrapper do
   end
 
   describe StripeWrapper::Charge do
-    before(:each) do
-      StripeWrapper.set_api_key
-    end
-
-    let(:token) do
-      Stripe::Token.create(
-        :card => {
-          :number => card_number,
-          :exp_month => 8,
-          :exp_year => 2016,
-          :cvc => "314"
-        },
-      ).id
-    end
-
-    context 'valid card' do
-      let(:card_number) { '4242424242424242' }
-
-      it 'charges successfully', :vcr do
-        response = StripeWrapper::Charge.create(amount: 999, token: token, user_email: 'stripetest@example.com')
-        expect(response).to be_successful
-      end
-    end
-
-    context 'invalid card' do
-      let(:card_number) { '4000000000000002' }
-
-      it 'does not charge the card', :vcr do
-        response = StripeWrapper::Charge.create(amount: 999, token: token, user_email: 'stripetest@example.com')
-        expect(response).to_not be_successful
+    describe '.charge' do
+      before(:each) do
+        StripeWrapper.set_api_key
       end
 
-      it 'has an error message for the end user', :vcr do
-        response = StripeWrapper::Charge.create(amount: 999, token: token, user_email: 'stripetest@example.com')
-        expect(response.error_message).to be_present
+      let(:token) do
+        Stripe::Token.create(
+          :card => {
+            :number => card_number,
+            :exp_month => 8,
+            :exp_year => 2016,
+            :cvc => "314"
+          },
+        ).id
+      end
+
+      context 'valid card' do
+        let(:card_number) { '4242424242424242' }
+
+        it 'charges successfully', :vcr do
+          response = StripeWrapper::Charge.create(amount: 999, token: token, user_email: 'stripetest@example.com')
+          expect(response).to be_successful
+        end
+      end
+
+      context 'invalid card' do
+        let(:card_number) { '4000000000000002' }
+
+        it 'does not charge the card', :vcr do
+          response = StripeWrapper::Charge.create(amount: 999, token: token, user_email: 'stripetest@example.com')
+          expect(response).to_not be_successful
+        end
+
+        it 'has an error message for the end user', :vcr do
+          response = StripeWrapper::Charge.create(amount: 999, token: token, user_email: 'stripetest@example.com')
+          expect(response.error_message).to be_present
+        end
       end
     end
   end
